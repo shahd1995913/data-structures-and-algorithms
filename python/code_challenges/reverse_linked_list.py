25d93c5abd129d9e91045c5c5d1bc21aebd2262a
shahd1995913 <shahdalkhatib95@gmail.com>
2021-10-10 16:59:37 +0300

    first=day

--- python/code_challenges/reverse_linked_list.py
+++ python/code_challenges/reverse_linked_list.py
@@ -1,9 +1,13 @@
 def reverse_list(ll):
-    """Reverses a linked list
-    Args:
-        ll: linked list
-    Returns:
-        linked list in reversed form
-    """
+    print(ll[::-1])
+    # return reverse_list(ll)
+
+reverse_list([1,2,3,4,5,6])
+
+
     # put your function implementation here
-    return ll
+
+
+
+
+
