ab7e0f7ff5286ed8cbb6d1d84106026635f2b220
shahd1995913 <shahdalkhatib95@gmail.com>
2021-10-10 21:22:55 +0300

    update

--- python/code_challenges/reverse_linked_list.py
+++ python/code_challenges/reverse_linked_list.py
@@ -1,11 +1,17 @@
-def reverse_list(ll):
-    print(ll[::-1])
-    # return reverse_list(ll)
+# the defintion of the function named reverse_list
 
+def reverse_list(arrayOfNum):
+    print(arrayOfNum[::-1])
+
+
+
+# return reverse_list(arrayOfNum)
+print("The result will be  after reverse :")
 reverse_list([1,2,3,4,5,6])
 
 
-    # put your function implementation here
+# put your function implementation here
+# I make a python sequncess slice by use array[start,stop,step]
 
 
 
