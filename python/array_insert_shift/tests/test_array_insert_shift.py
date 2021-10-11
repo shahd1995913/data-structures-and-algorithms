7e9138ae5f4bdb5d40a859b1adcfbda470d7f225
shahd1995913 <shahdalkhatib95@gmail.com>
2021-10-11 23:33:47 +0300

    update1

--- python/array_insert_shift/tests/test_array_insert_shift.py
+++ python/array_insert_shift/tests/test_array_insert_shift.py
@@ -1,5 +1,12 @@
-from array_insert_shift import __version__
+# from array_insert_shift import __version__
 
+# from array_insert_shift.array_insert_shift import __finsertShiftArray___
 
-def test_version():
-    assert __version__ == '0.1.0'
+# def test_version():
+#     assert __version__ == '0.1.0'
+
+# def test_array():
+#     array=[2,4,6,-8]
+#     input1=5
+#     assert  __finsertShiftArray___(array,input1)==[2,4,5,6,-8]
+
