7cace6c76dbc35b7eba1ab323c6fa16d1124a1df
shahd1995913 <shahdalkhatib95@gmail.com>
2021-10-24 20:39:12 +0400

    update

---
+++ python/stack_queue_pseudo/README.md
@@ -0,0 +1,18 @@
+# Challenge Summary
+<!-- Description of the challenge -->
+Implement a Queue using two Stacks.
+## Whiteboard Process
+<!-- Embedded whiteboard image -->
+![image5](stack_queue_pseudo.png)
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+### I take approach in about understand how the binary search work and how to return the index that search .
+
+### I take 4 hrs to done all code .
+## Solution
+<!-- Show how to run your code, and examples of it in action -->
+### The result after run the code
+![image5](results.png)
+
+
+
---
+++ python/stack_queue_pseudo/stack_queue_pseudo.py
@@ -0,0 +1,94 @@
+"""
+Implement a Queue using two Stacks.
+
+
+"""
+
+
+class queue:
+
+
+    def __init__(self):
+
+
+        self.stack1 = []
+
+        self.stack2 = []
+
+
+    def enqueue(self, value):
+
+        """
+        transfer the data element from stack 1 to stack 2
+        """
+
+
+        while len(self.stack1) != 0:
+
+            self.stack2.append(self.stack1[-1])
+
+            self.stack1.pop()
+            """
+            Push  the element to stack 1
+            """
+
+
+
+
+
+        self.stack1.append(value)
+        """
+
+        Push the all data to stack 1 also
+        """
+
+
+
+
+        while len(self.stack2) != 0:
+
+            self.stack1.append(self.stack2[-1])
+
+
+            self.stack2.pop()
+
+            """
+            dequeue element from the queue
+
+            """
+
+
+    def dequeue(self):
+
+
+
+
+        if len(self.stack1) == 0:
+
+            print("Queue is not contain any data")
+
+
+        value = self.stack1[-1]
+
+        self.stack1.pop()
+
+        return value
+
+
+if __name__ == '__main__':
+
+    queu1 = queue()
+
+    queu1.enqueue(20)
+
+    queu1.enqueue(15)
+
+    queu1.enqueue(20)
+
+
+    print(queu1.dequeue())
+
+    print(queu1.dequeue())
+
+    print(queu1.dequeue())
+
