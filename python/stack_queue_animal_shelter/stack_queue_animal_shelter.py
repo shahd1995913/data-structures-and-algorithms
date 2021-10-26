933a8649bfcf869f99518b4ff4a4ae3ee6a758bd
shahd1995913 <shahdalkhatib95@gmail.com>
2021-10-26 11:06:08 +0400

    update

--- python/stack_queue_animal_shelter/stack_queue_animal_shelter.py
+++ python/stack_queue_animal_shelter/stack_queue_animal_shelter.py
@@ -1,15 +1,17 @@
 
+
+
 class Node:
     """ self reference to object like this  """
     """  class contian all element  , Reference for all  data called class Node  """
-    def __init__(self, value=None):
+    def __init__(self, value=None) :
         """ method called when an object is created from the class
         and it allow the class to initialize the attributes of a class"""
         self.value = value
 
         self.next = None
 
-class Queue:
+class Queue :
 
     def __init__(self):
 
@@ -17,15 +19,161 @@ class Queue:
         self.front = None
 
         self.rear = None
+
         """ represents the class objects as a string """
         """ This method is also used as a debugging tool when the members of a class need to be checked"""
+
     def __str__(self):
 
-        current = self.front
+        cur_node = self.front
 
-        items = []
+        l = []
 
-        while current:
+        while cur_node :
             """  add something to list python """
+
+            l.append(str(cur_node.value))
+
+            cur_node = cur_node.next
+
+        return " ".join(l)
+
+        """  The join() method takes all items in an iterable and joins them into one string. """
+
+    def enqueue(self, val):
+        """Enqueue: Adds an item to the queue """
+        node_enqueue = Node(val)
+
+        if not self.front:
+
+            self.front = node_enqueue
+
+            self.rear = node_enqueue
+
+        else:
+
+            self.rear.next = node_enqueue
+
+            self.rear = self.rear.next
+
+    def dequeue(self):
+
+        """  deque, has the feature of adding and removing elements from either end """
+
+        if not self.is_empty():
+
+            dequeue_node = self.front
+
+            self.front = self.front.next
+
+            dequeue_node.next = None
+
+            return dequeue_node.value
+
+    def is_empty(self):
+
+        if self.front:
+
+            return False
+
+        return True
+
+class AnimalShelter():
+
+    def __init__(self):
+
+        self.cat = Queue()
+
+        self.dog = Queue()
+
+    def enqueue(self, a):
+
+        if a.typeofanamal == "cat":
+
+          self.cat.enqueue(a.name)
+
+        elif a.typeofanamal == "dog":
+
+          self.dog.enqueue(a.name)
+
+    def dequeue(self, pref):
+
+        if pref == "cat":
+
+          self.cat.dequeue()
+
+        elif pref == "dog":
+
+          self.dog.dequeue()
+
+        else:
+
+          return "null"
+
+class cat_animal():
+
+  def __init__(self,name):
+
+    self.name = name
+
+    self.typeofanamal = "cat"
+
+class dog_animal():
+
+  def __init__(self,name):
+
+    self.name = name
+
+    self.typeofanamal = "dog"
+
+
+if __name__ == "__main__":
+
+    shelter = AnimalShelter()
+
+    dog1 = dog_animal("Ram")
+
+    dog2 = dog_animal("Jo")
+
+    dog3 = dog_animal("Li")
+
+    dog4  = dog_animal("Miro")
+
+    shelter.enqueue(dog1)
+
+    shelter.enqueue(dog2)
+
+    shelter.enqueue(dog3)
+
+    shelter.enqueue(dog4)
+
+
+    print(shelter.dog)
+
+    shelter.dequeue("dog")
+
+    print(shelter.dog)
+
+    cat1 = cat_animal("lili")
+
+    cat2 = cat_animal("meaow")
+
+    cat3 = cat_animal("tom")
+
+    cat4 = cat_animal("jury")
+
+    shelter.enqueue(cat1)
+
+    shelter.enqueue(cat2)
+
+    shelter.enqueue(cat3)
+
+    shelter.enqueue(cat4)
+
+    print(shelter.cat)
+
+    shelter.dequeue("cat")
+
+    print(shelter.cat)
 
 
